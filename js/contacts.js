9cd4a21a27869dd4a7e9eb4925f30de3d4a9709c
Fedois <fedefox14@gmail.com>
2023-01-26 10:53:56 +0100

    aggiunta contatti

--- js/contacts.js
+++ js/contacts.js
@@ -83,81 +83,81 @@ contacts: [
         ],
     },
     {
-    name: 'Alessandro L.',
-    avatar: '_5',
-    visible: true,
-    messages: [
+        name: 'Alessandro L.',
+        avatar: '_5',
+        visible: true,
+        messages: [
     {
-    date: '10/01/2020 15:30:55',
-    message: 'Ricordati di chiamare la nonna',
-    status: 'sent'
+            date: '10/01/2020 15:30:55',
+            message: 'Ricordati di chiamare la nonna',
+            status: 'sent'
+            },
+            {
+            date: '10/01/2020 15:50:00',
+            message: 'Va bene, stasera la sento',
+            status: 'received'
+            }
+        ],
     },
     {
-    date: '10/01/2020 15:50:00',
-    message: 'Va bene, stasera la sento',
-    status: 'received'
-    }
-    ],
+        name: 'Claudia',
+        avatar: '_6',
+        visible: true,
+        messages: [
+            {
+            date: '10/01/2020 15:30:55',
+            message: 'Ciao Claudia, hai novità?',
+            status: 'sent'
+            },
+            {
+            date: '10/01/2020 15:50:00',
+            message: 'Non ancora',
+            status: 'received'
+            },
+            {
+            date: '10/01/2020 15:51:00',
+            message: 'Nessuna nuova, buona nuova',
+            status: 'sent'
+            }
+        ],
     },
     {
-    name: 'Claudia',
-    avatar: '_6',
-    visible: true,
-    messages: [
-    {
-    date: '10/01/2020 15:30:55',
-    message: 'Ciao Claudia, hai novità?',
-    status: 'sent'
+        name: 'Federico',
+        avatar: '_7',
+        visible: true,
+        messages: [
+            {
+            date: '10/01/2020 15:30:55',
+            message: 'Fai gli auguri a Martina che è il suo compleanno!',
+            status: 'sent'
+            },
+            {
+            date: '10/01/2020 15:50:00',
+            message: 'Grazie per avermelo ricordato, le scrivo subito!',
+            status: 'received'
+            }
+        ],
     },
     {
-    date: '10/01/2020 15:50:00',
-    message: 'Non ancora',
-    status: 'received'
-    },
-    {
-    date: '10/01/2020 15:51:00',
-    message: 'Nessuna nuova, buona nuova',
-    status: 'sent'
-    }
-    ],
-    },
-    {
-    name: 'Federico',
-    avatar: '_7',
-    visible: true,
-    messages: [
-    {
-    date: '10/01/2020 15:30:55',
-    message: 'Fai gli auguri a Martina che è il suo compleanno!',
-    status: 'sent'
-    },
-    {
-    date: '10/01/2020 15:50:00',
-    message: 'Grazie per avermelo ricordato, le scrivo subito!',
-    status: 'received'
-    }
-    ],
-    },
-    {
-    name: 'Davide',
-    avatar: '_8',
-    visible: true,
-    messages: [
-    {
-    date: '10/01/2020 15:30:55',
-    message: 'Ciao, andiamo a mangiare la pizza stasera?',
-    status: 'received'
-    },
-    {
-    date: '10/01/2020 15:50:00',
-    message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
-    status: 'sent'
-    },
-    {
-    date: '10/01/2020 15:51:00',
-    message: 'OK!!',
-    status: 'received'
-    }
+        name: 'Davide',
+        avatar: '_8',
+        visible: true,
+        messages: [
+            {
+            date: '10/01/2020 15:30:55',
+            message: 'Ciao, andiamo a mangiare la pizza stasera?',
+            status: 'received'
+            },
+            {
+            date: '10/01/2020 15:50:00',
+            message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
+            status: 'sent'
+            },
+            {
+            date: '10/01/2020 15:51:00',
+            message: 'OK!!',
+            status: 'received'
+            }
     ],
     }
 ]
