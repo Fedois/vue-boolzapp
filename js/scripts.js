7f6d141b7cddc818260d3bd7c231f3dda8315f54
Fedois <fedefox14@gmail.com>
2023-01-27 15:03:39 +0100

    aggiunta milestone 5

--- js/scripts.js
+++ js/scripts.js
@@ -18,16 +18,19 @@ data() {
                     {
                     date: '10/01/2020 15:30:55',
                     message: 'Hai portato a spasso il cane?',
+                    viewMex: false,
                     status: 'sent'
                     },
                     {
                     date: '10/01/2020 15:50:00',
                     message: 'Ricordati di stendere i panni',
+                    viewMex: false,
                     status: 'sent'
                     },
                     {
                     date: '10/01/2020 16:15:22',
                     message: 'Tutto fatto!',
+                    viewMex: false,
                     status: 'received'
                     }
                 ],
@@ -40,16 +43,19 @@ data() {
                     {
                     date: '20/03/2020 16:30:00',
                     message: 'Ciao come stai?',
+                    viewMex: false,
                     status: 'sent'
                     },
                     {
                     date: '20/03/2020 16:30:55',
                     message: 'Bene grazie! Stasera ci vediamo?',
+                    viewMex: false,
                     status: 'received'
                     },
                     {
                     date: '20/03/2020 16:35:00',
                     message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                    viewMex: false,
                     status: 'sent'
                     }
                 ],
@@ -62,16 +68,19 @@ data() {
                     {
                     date: '28/03/2020 10:10:40',
                     message: 'La Marianna va in campagna',
+                    viewMex: false,
                     status: 'received'
                     },
                     {
                     date: '28/03/2020 10:20:10',
                     message: 'Sicuro di non aver sbagliato chat?',
+                    viewMex: false,
                     status: 'sent'
                     },
                     {
                     date: '28/03/2020 16:15:22',
                     message: 'Ah scusa!',
+                    viewMex: false,
                     status: 'received'
                     }
                 ],
@@ -84,11 +93,13 @@ data() {
                     {
                     date: '10/01/2020 15:30:55',
                     message: 'Lo sai che ha aperto una nuova pizzeria?',
+                    viewMex: false,
                     status: 'sent'
                     },
                     {
                     date: '10/01/2020 15:50:00',
                     message: 'Si, ma preferirei andare al cinema',
+                    viewMex: false,
                     status: 'received'
                     }
                 ],
@@ -101,11 +112,13 @@ data() {
             {
                     date: '10/01/2020 15:30:55',
                     message: 'Ricordati di chiamare la nonna',
+                    viewMex: false,
                     status: 'sent'
                     },
                     {
                     date: '10/01/2020 15:50:00',
                     message: 'Va bene, stasera la sento',
+                    viewMex: false,
                     status: 'received'
                     }
                 ],
@@ -118,16 +131,19 @@ data() {
                     {
                     date: '10/01/2020 15:30:55',
                     message: 'Ciao Claudia, hai novità?',
+                    viewMex: false,
                     status: 'sent'
                     },
                     {
                     date: '10/01/2020 15:50:00',
                     message: 'Non ancora',
+                    viewMex: false,
                     status: 'received'
                     },
                     {
                     date: '10/01/2020 15:51:00',
                     message: 'Nessuna nuova, buona nuova',
+                    viewMex: false,
                     status: 'sent'
                     }
                 ],
@@ -140,11 +156,13 @@ data() {
                     {
                     date: '10/01/2020 15:30:55',
                     message: 'Fai gli auguri a Martina che è il suo compleanno!',
+                    viewMex: false,
                     status: 'sent'
                     },
                     {
                     date: '10/01/2020 15:50:00',
                     message: 'Grazie per avermelo ricordato, le scrivo subito!',
+                    viewMex: false,
                     status: 'received'
                     }
                 ],
@@ -157,16 +175,19 @@ data() {
                     {
                     date: '10/01/2020 15:30:55',
                     message: 'Ciao, andiamo a mangiare la pizza stasera?',
+                    viewMex: false,
                     status: 'received'
                     },
                     {
                     date: '10/01/2020 15:50:00',
                     message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
+                    viewMex: false,
                     status: 'sent'
                     },
                     {
                     date: '10/01/2020 15:51:00',
                     message: 'OK!!',
+                    viewMex: false,
                     status: 'received'
                     }
             ],
@@ -182,6 +203,7 @@ methods: {
         this.contacts[this.currentContact].messages.push({
             date: hour.getHours() + ':' + hour.getMinutes(),
             message: this.writingMex, 
+            viewMex: false,
             status: 'sent'
         }),
         
@@ -192,30 +214,27 @@ methods: {
             this.contacts[this.currentContact].messages.push({
                 date: hour.getHours() + ':' + hour.getMinutes(),
                 message: this.reply, 
+                viewMex: false,
                 status: 'received'
             })
             
         }, 1000);
     },
-    addMexDelete(){
-        if(this.display == 'd-block'){
-            this.display = ''
-        }
-        else{
-            this.display = 'd-block'
-        }
-    },
-    
-    splitDate(message){
-        let splitDate = message.date
-        const splitted = splitDate.split(' ')
-        splitDate = splitted[1];
 
-        let splitHour = splitDate.split(':')
-        console.log(splitHour)
-        const removeSec = splitHour[0] + ':' + splitHour[1]
-        return removeSec
-    },
+    deletedMessages: function(index){
+        this.contacts[this.currentContact].messages.splice(index, 1)
+        console.log(index)
+    }
+    
+    // splitDate(message){
+    //     let splitDate = message.date
+    //     const splitted = splitDate.split(' ')
+    //     splitDate = splitted[1];
+
+    //     let splitHour = splitDate.split(':')
+    //     const removeSec = splitHour[0] + ':' + splitHour[1]
+    //     return removeSec
+    // },
 },
 
 computed: {
